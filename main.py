24659fb38f0fa747746b90a5d24dd16624b3b24d
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-03 21:09:46 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+def main():
+    # Ubuntu version 10.10
+    os.system('clear')
+
+    print("Hello to the greatest algorithm project ever!\n"
+            "We have certain algorithms ready for you to test out\n"
+            "Please select any one of the following below algorithms\n" 
+            "and then choose the related datasets that will appear as expected\n")
+
+
+if __name__ == "__main__":
+    main()
