833f378d33ee3778f3dee2685a21822eee9723ea
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-15 22:30:01 +0500

    changed location for src files

---
+++ apps/base/src/word_break.py
@@ -0,0 +1,35 @@
+# Function to segment given into a space-separated
+# sequence of one or more dictionary words
+def wordBreak(dict, str, out=""):
+ 
+    # if we have reached the end of the String,
+    # print the output String
+    if not str:
+        print(out)
+        return
+ 
+    for i in range(1, len(str) + 1):
+        # consider all prefixes of current String
+        prefix = str[:i]
+ 
+        # if the prefix is present in the dictionary, add prefix to the
+        # output and recur for remaining String
+        if prefix in dict:
+            wordBreak(dict, str[i:], out + " " + prefix)
+ 
+# Driver Code
+# if __name__ == '__main__':
+ 
+#     # List of Strings to represent dictionary
+#     dict = [
+#         "self", "th", "is", "famous", "Word",
+#         "break", "b", "r", "e", "a", "k", "br",
+#         "bre", "brea", "ak", "problem"
+#     ]
+ 
+#     # input String
+#     str = "Wordbreakproblem"
+ 
+#     wordBreak(dict, str)
+ 
+# See more details on https://www.techiedelight.com/word-break-problem/
