67679b4ef448570726b643609e70184ec319d045
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-15 22:30:44 +0500

    home template changes, home view made proper

--- apps/base/views.py
+++ apps/base/views.py
@@ -2,26 +2,36 @@
 
 from django.shortcuts import render
 
+from .src.coin_change_making import countBottomUp
+
 
 def home(request):
+
+    list_of_algorithms = ['Coin Change Making', 'Knapsack 01', 'Levenshtein Distance', 'Longest Increasing Subsequence', 'Longest Common Subsequence',
+                          'Matrix Chain Multiplication', 'Partition Problem - Dynamic', 'Rod Cutting Problem', 'Shortest Common Super Sequence', 'Work Break Problem']
+
+    context = {}
+    context['algorithms'] = list_of_algorithms
+
     """ Default view for the root """
-    return render(request, 'base/home.html')
+    return render(request, 'home.html', context)
 
 
 def report(request):
     ''' Report view '''
-    return render(request, 'base/report.html')
+    return render(request, 'report.html')
 
 
 def data(request):
     ''' Data view '''
-    return render(request, 'base/data.html')
+    return render(request, 'data.html')
 
 
 def contact(request):
     ''' Contact view '''
-    return render(request, 'base/contact.html')
+    return render(request, 'contact.html')
+
 
 def about(request):
     ''' About view '''
-    return render(request, 'base/about.html')
+    return render(request, 'about.html')
