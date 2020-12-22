d8a51c0230f3a75f19887d5d8031d1fab25d13ef
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-23 03:06:19 +0500

    refactor coin change

--- apps/base/src/coin_change_making.py
+++ apps/base/src/coin_change_making.py
@@ -1,49 +1,89 @@
-# Dynamic Programming Python implementation of Coin
-# Change problem
-def countMemoized(S, m, n):
-    # We need n+1 rows as the table is constructed
-    # in bottom up manner using the base case 0 value
-    # case (n = 0)
-    table = [[0 for x in range(m)] for x in range(n+1)]
+# DP implementation for Coin Change problem
+def countMemoized(set_of_given_coins: list,
+                  length_of_set_of_given_coins: int,
+                  number_of_cents: int) -> int:
+    '''
+    Returns a memoized table taking care of the coin changes possible
+
+    Parameters:
+    - @param: set_of_given_coins - This is a given set of the input array for 
+    the set of coins that we have to make change for
+    - @param: length_of_set_of_given_coins - simply an integer denoting 
+    - @param: number_of_cents - the total number of cents we can make 
+    changes with
+
+    Returns:
+    - The last obtained result in the memoized table created
+    '''
+
+    # We would need to create n+1 rows as the table is constructed
+    # in bottom up manner using the base case 0 value case (n = 0)
+    memoized_table = [[0 for iterator in range(
+        length_of_set_of_given_coins)] for iterator in range(number_of_cents+1)]
 
     # Fill the entries for 0 value case (n = 0)
-    for i in range(m):
-        table[0][i] = 1
+    for i in range(length_of_set_of_given_coins):
+        memoized_table[0][i] = 1
 
-    # Fill rest of the table entries in bottom up manner
-    for i in range(1, n+1):
-        for j in range(m):
+    # Fill rest of the memoized_table entries in bottom up manner
+    for ith_row in range(1, number_of_cents+1):
+        for jth_col in range(length_of_set_of_given_coins):
 
-            # Count of solutions including S[j]
-            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+            # Count of solutions including set_of_given_coins[j]
+            if ith_row-set_of_given_coins[jth_col] >= 0:
+                x = memoized_table[ith_row - set_of_given_coins[jth_col]]
+            else:
+                x = 0
 
-            # Count of solutions excluding S[j]
-            y = table[i][j-1] if j >= 1 else 0
+            # Count of solutions excluding set_of_given_coins[j]
+            if jth_col >= 1:
+                y = memoized_table[ith_row][jth_col-1]
+            else:
+                y = 0
 
             # total count
-            table[i][j] = x + y
+            memoized_table[ith_row][jth_col] = x + y
 
-    return table[n][m-1]
+    # Return the last obtained result
+    return memoized_table[number_of_cents][length_of_set_of_given_coins-1]
 
-def countBottomUp(S, m, n):
 
-    # table[i] will be storing the number of solutions for
-    # value i. We need n+1 rows as the table is constructed
+def countBottomUp(set_of_given_coins: list,
+                  length_of_set_of_given_coins: int,
+                  number_of_cents: int) -> list:
+    '''
+    Performs the countBottomUp operation, for performing the Coin
+    Change problem in a bottom up fashion
+
+    Parameters:
+    - @param: set_of_given_coins - This is a given set of the input array for 
+    the set of coins that we have to make change for
+    - @param: length_of_set_of_given_coins - simply an integer denoting 
+    - @param: number_of_cents - the total number of cents we can make 
+    changes with
+
+    Returns:
+    - The last obtained result in the memoized table created
+    '''
+
+    # Memoized_table[i] will be storing the number of solutions for
+    # value i. We need n+1 rows as the memoized_table is constructed
     # in bottom up manner using the base case (n = 0)
-    # Initialize all table values as 0
-    table = [0 for k in range(n+1)]
+    # Initialize all memoized_table values as 0
+    memoized_table = [0 for k in range(number_of_cents+1)]
 
-    # Base case (If given value is 0)
-    table[0] = 1
+    # Base case if given value is 0
+    memoized_table[0] = 1
 
-    # Pick all coins one by one and update the table[] values
+    # Pick all coins one by one and update the memoized_table[] values
     # after the index greater than or equal to the value of the
     # picked coin
-    for i in range(0, m):
-        for j in range(S[i], n+1):
-            table[j] += table[j-S[i]]
+    for ith_row in range(0, length_of_set_of_given_coins):
+        for jth_col in range(set_of_given_coins[ith_row], number_of_cents+1):
+            memoized_table[jth_col] += memoized_table[jth_col -
+                                                      set_of_given_coins[ith_row]]
 
-    return table[n]
+    return memoized_table[number_of_cents]
 
 # Driver program to test above function
 # arr = [1, 2, 3]
