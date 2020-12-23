52b1e68f81aded3206b154bca7623b122709ce83
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-23 21:34:33 +0500

    add styling and js for the page

--- cs302AlgorithmsProject/static/js/app.js
+++ cs302AlgorithmsProject/static/js/app.js
@@ -1,7 +1,5 @@
 let allDatasets = (document.querySelectorAll(`ol#datasets.gradient-list>li`))
 let allAlgorithms = (document.querySelectorAll(`ol#algorithms.gradient-list>li`))
-// console.log(allDatasets);
-// console.log(allAlgorithms);
 let algorithm = '';
 let dataset = '';
 
@@ -45,39 +43,77 @@ function set_dataset(event) {
 function submit_button() {
 
     if (algorithm !== '' && dataset !== '') {
-        console.log(`Algorithm is ${algorithm}`)
-        console.log(`Dataset is ${dataset}`)
-
-        if (algorithm === 0 || algorithm === 1 || algorithm === 2) {
-            if (dataset === 0 || dataset === 1 || dataset === 3) {
-
+        if (algorithm === '0' || algorithm === '1' || algorithm === '2') {
+            if (dataset === '0' || dataset === '1' || dataset === '3') {
+                window.location.replace(`${window.location.href}result/${algorithm}/${dataset}`)
             } else {
                 alert(`Invalid dataset chosen. You can choose either 
-                the 1st, 2nd, or 3rd dataset to go with the algorithm you have chosen`)
+the 1st, 2nd, or 3rd dataset to go with the algorithm you have chosen`)
             }
-        } else if (algorithm === 3 || algorithm === 4 || algorithm === 8) {
-            if (dataset === 3 || dataset === 4 || dataset === 8) {
-
+        } else if (algorithm === '3' || algorithm === '4' || algorithm === '8') {
+            if (dataset === '3' || dataset === '4' || dataset === '8') {
+                window.location.replace(`${window.location.href}result/${algorithm}/${dataset}`)
             } else {
                 alert(`Invalid dataset chosen. You can choose either 
-                the 4th, 5th, or the 9th dataset to go with the algorithm you have chosen`)
+the 4th, 5th, or the 9th dataset to go with the algorithm you have chosen`)
             }
-        } else if (algorithm === 5 || algorithm === 6 || algorithm === 7) {
-            if (dataset === 5 || dataset === 6 || dataset === 7) {
-
+        } else if (algorithm === '5' || algorithm === '6' || algorithm === '7') {
+            if (dataset === '5' || dataset === '6' || dataset === '7') {
+                window.location.replace(`${window.location.href}result/${algorithm}/${dataset}`)
             } else {
                 alert(`Invalid dataset chosen. You can choose either 
-                the 6th, 7th, or 8th dataset to go with the algorithm you have chosen`)
+the 6th, 7th, or 8th dataset to go with the algorithm you have chosen`)
             }
-        } else if (algorithm === 9) {
-            if (dataset === 9) {
-
+        } else if (algorithm === '9') {
+            if (dataset === '9') {
+                window.location.replace(`${window.location.href}result/${algorithm}/${dataset}`)
             } else {
                 alert(`Invalid dataset chosen. Only the 10th dataset can 
-                be chosen to go with the algorithm you have chosen`)
+be chosen to go with the algorithm you have chosen`)
             }
         }
     } else {
         alert('Please choose the options from both the lists')
     }
+}
+
+if (window.location.href.includes('/result/')) {
+    let hrefList = window.location.href.split('/')
+    let lengthOfHref = hrefList.length
+    let dataset = hrefList[lengthOfHref - 1];
+    let algorithm = hrefList[lengthOfHref - 2];
+    let imageHandler = document.querySelector('.render-image-for-code').src;
+    let findIndexFor = 'coin_change_making.png';
+    let indexForSubStr = imageHandler.indexOf(findIndexFor);
+    let extractURI = imageHandler.substr(0, indexForSubStr - 1);
+    let newResource = '';
+
+    console.log(`Algorithm is ${algorithm}`)
+    console.log(`Dataset is ${dataset}`)
+
+    if (algorithm === '0') {
+        newResource = 'coin_change_making.png';
+    } else if (algorithm === '1') {
+        newResource = 'knapsack_01.png';
+    } else if (algorithm === '2') {
+        newResource = 'levensthein.png';
+    } else if (algorithm === '3') {
+        newResource = 'lis.png';
+    } else if (algorithm === '4') {
+        newResource = 'longest_common_subsequence.png';
+    } else if (algorithm === '5') {
+        newResource = 'matrix_chain_multiplication.png';
+    } else if (algorithm === '6') {
+        newResource = 'partition.png';
+    } else if (algorithm === '7') {
+        newResource = 'rod_cutting.png';
+    } else if (algorithm === '8') {
+        newResource = 'shortest_common_supersequence.png';
+    } else if (algorithm === '9') {
+        newResource = 'word_break.png';
+    }
+
+    extractURI = `${extractURI}/${newResource}`;
+
+    document.querySelector('.render-image-for-code').src = extractURI;
 }
